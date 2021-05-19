86053facc4e5129fe762dd637ca8fa9cb39273cf
Hungjui Yu <yuhungjui@gmail.com>
2021-05-19 10:17:54 -0600

    clear test files

--- ST_L0_L1.py
+++
@@ -1,8 +0,0 @@
-"""
-Load Storm Tracker raw data (L0) and transform it into .csv format file (L1).
-Hungjui Yu, Apr. 26, 2021.
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
-import xarray as xr
